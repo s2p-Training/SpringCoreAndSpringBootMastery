864e8b59a2b4d47eab83b42533d8546c63885f36
pranayramteke <pranayramteke613@gmail.com>
2025-11-14 14:47:22 +0530

    Wiring Beans Using Method Parameters

---
+++ section11/src/main/java/org/s2p/beans/Vehicle.java
@@ -0,0 +1,21 @@
+package org.s2p.beans;
+
+
+public class Vehicle {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle name is " + this.name;
+    }
+
+}
