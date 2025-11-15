a8ee7423bc7ac7db2e0178ffb5957cbcd0a69b02
pranayramteke <pranayramteke613@gmail.com>
2025-11-15 21:11:53 +0530

    Wiring Beans Using @Qualifier Annotation

---
+++ section14/src/main/java/org/s2p/main/ApplicationRunner.java
@@ -0,0 +1,16 @@
+package org.s2p.main;
+
+import org.s2p.beans.Person;
+import org.s2p.beans.Vehicle;
+import org.s2p.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationRunner {
+    public static void main(String[] args)
+    {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Person person   = context.getBean(Person.class);
+        System.out.println("Person name from Spring Context is : " + person.getName());
+        System.out.println("Vehicle that person owns is : " + person.getVehicle());
+    }
+}
