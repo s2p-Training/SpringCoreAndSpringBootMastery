c252fff0eded471756f666b919338393dafc7898
pranayramteke <pranayramteke613@gmail.com>
2025-12-30 17:57:21 +0530

    Added Section23 With Cors Configuration

--- section23/src/main/java/org/s2p/controller/ProductController.java
+++ section23/src/main/java/org/s2p/controller/ProductController.java
@@ -1,10 +1,5 @@
 package org.s2p.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.s2p.dto.ProductDto;
-import org.s2p.service.IProductService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,17 +7,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/products")
-@RequiredArgsConstructor
 public class ProductController
 {
-    private final IProductService productService;
 
-//    @CrossOrigin(origins = {"http://localhost:5173"})
-    @GetMapping
-    public List<ProductDto> getProducts()
+    public String getProducts()
     {
         System.out.println("Hello Products API");
-        List<ProductDto> productDtoList =  productService.getAllProducts();
-        return productDtoList;
+       return "Here Is The List Of Products";
     }
 }
