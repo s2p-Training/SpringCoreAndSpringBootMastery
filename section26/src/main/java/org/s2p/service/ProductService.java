207a75ea369b21e4303e094e22be34da8fa69f39
pranayramteke <pranayramteke613@gmail.com>
2026-01-08 10:08:19 +0530

    CORS Configuration

---
+++ section26/src/main/java/org/s2p/service/ProductService.java
@@ -0,0 +1,11 @@
+package org.s2p.service;
+
+import org.s2p.dto.ProductDto;
+import org.s2p.entity.Product;
+
+import java.util.List;
+
+public interface ProductService
+{
+    List<ProductDto> fetchAllProducts();
+}
